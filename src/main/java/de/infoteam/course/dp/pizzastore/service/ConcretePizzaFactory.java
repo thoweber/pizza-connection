39a8f1b20d20f9d7e4fed648961391ade21829a6
Thomas Weber <thomas.weber@infoteam.de>
2025-06-13 14:51:54 +0200

    Kapitel 2 vorbereitet

--- src/main/java/de/infoteam/course/dp/pizzastore/service/ConcretePizzaFactory.java
+++ src/main/java/de/infoteam/course/dp/pizzastore/service/ConcretePizzaFactory.java
@@ -2,19 +2,15 @@ package de.infoteam.course.dp.pizzastore.service;
 
 import de.infoteam.course.dp.pizzastore.model.MenuItem;
 import de.infoteam.course.dp.pizzastore.model.Pizza;
-import de.infoteam.course.dp.pizzastore.model.dishes.CheesePizza;
-import de.infoteam.course.dp.pizzastore.model.dishes.PeperoniPizza;
-import de.infoteam.course.dp.pizzastore.model.dishes.VeggiePizza;
 
 public class ConcretePizzaFactory implements PizzaFactory {
 
 	@Override
 	public Pizza createPizza(MenuItem selectedItem) {
-		return switch (selectedItem) {
-			case CHEESE_PIZZA -> new CheesePizza();
-			case PEPERONI_PIZZA -> new PeperoniPizza();
-			case VEGGIE_PIZZA -> new VeggiePizza();
-		};
+		/*
+		 * Ersetze den Code dieser Methode durch deine Factory-Implementierung
+		 */
+		return null;
 	}
 
 }
