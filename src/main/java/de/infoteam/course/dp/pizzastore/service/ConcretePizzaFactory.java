258e6e8205f965eba4a70b8b070e74b17c137c83
Thomas Weber <thomas.weber@infoteam.de>
2025-06-13 15:43:10 +0200

    Lösung für Kapitel 2

--- src/main/java/de/infoteam/course/dp/pizzastore/service/ConcretePizzaFactory.java
+++ src/main/java/de/infoteam/course/dp/pizzastore/service/ConcretePizzaFactory.java
@@ -2,15 +2,19 @@ package de.infoteam.course.dp.pizzastore.service;
 
 import de.infoteam.course.dp.pizzastore.model.MenuItem;
 import de.infoteam.course.dp.pizzastore.model.Pizza;
+import de.infoteam.course.dp.pizzastore.model.dishes.CheesePizza;
+import de.infoteam.course.dp.pizzastore.model.dishes.PeperoniPizza;
+import de.infoteam.course.dp.pizzastore.model.dishes.VeggiePizza;
 
 public class ConcretePizzaFactory implements PizzaFactory {
 
-  @Override
-  public Pizza createPizza(MenuItem selectedItem) {
-    /*
-     * Ersetze den Code dieser Methode durch deine Factory-Implementierung
-     */
-    throw new UnsupportedOperationException(
-        "Ersetze den Code dieser Methode durch deine Factory-Implementierung");
-  }
+	@Override
+	public Pizza createPizza(MenuItem selectedItem) {
+		return switch (selectedItem) {
+			case CHEESE_PIZZA -> new CheesePizza();
+			case PEPERONI_PIZZA -> new PeperoniPizza();
+			case VEGGIE_PIZZA -> new VeggiePizza();
+		};
+	}
+
 }
