e8088cf10126ec680872b752372845050ebee006
Thomas Weber <thomas.weber@infoteam.de>
2025-09-30 19:36:48 +0200

    Kapitel 2: Code Clean-Up

--- src/main/java/de/infoteam/course/dp/pizzastore/model/ingredients/toppings/TomatoTopping.java
+++ src/main/java/de/infoteam/course/dp/pizzastore/model/ingredients/toppings/TomatoTopping.java
@@ -2,11 +2,10 @@ package de.infoteam.course.dp.pizzastore.model.ingredients.toppings;
 
 public class TomatoTopping implements Topping {
 
-	private static final String NAME = "freshly sliced tomato";
+  private static final String NAME = "freshly sliced tomato";
 
-	@Override
-	public String name() {
-		return TomatoTopping.NAME;
-	}
-
+  @Override
+  public String name() {
+    return TomatoTopping.NAME;
+  }
 }
